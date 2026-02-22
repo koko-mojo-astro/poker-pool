61504d7097e7dfbd63782ee7c8f64f0d37f91e9d
koko-mojo-astro <tunmin.koko305@gmail.com>
2026-02-22 19:27:48 +1300

    chore: move InstantDB app ID to environment variable and update gitignore

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,6 @@
 import { init } from '@instantdb/react';
 import schema from '../instant.schema';
 
-const APP_ID = 'febc5aac-8f94-4fd0-99ee-9f6565721ef2';
+const APP_ID = import.meta.env.VITE_INSTANT_APP_ID;
 
 export const db = init({ appId: APP_ID, schema });
